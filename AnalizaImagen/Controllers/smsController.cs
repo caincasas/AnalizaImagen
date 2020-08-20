9a379384e8e58e21851a0e9827dadf00c87ddd9a
CAIN <cain.casas@maincasa.com>
2020-08-20 17:29:31 -0500

    Inicio
    Inicio del proyecto

---
+++ AnalizaImagen/Controllers/smsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+using Task = System.Threading.Tasks.Task;
+
+namespace AnalizaImagen.Controllers
+{
+    public class smsController : TwilioController
+    {
+        // GET: sms
+        [HttpPost]
+        public async Task<TwiMLResult> Index(SmsRequest request, int numMedia)
+        {
+            string respuesta = "";
+            var response = new MessagingResponse();
+            string urlImagen = numMedia > 0 ? Request.Form[$"MediaUrl0"] : "";
+            if (urlImagen == "")
+            {
+                respuesta = "Por favor envía una imagen para analizar. 🤖👀";
+            }
+            else
+            {
+                respuesta = "Url de imagen: \n" + urlImagen;
+            }
+            response.Message(respuesta);
+            return TwiML(response);
+        }
+    }
+}
